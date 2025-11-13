66ae39705f81c5cbcfab4fa167cd19af10e87538
MarioLivimoro <jcvdbocanegra@gmail.com>
2025-11-12 21:42:03 -0500

    segunda modificacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import "./App.css";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Listing from "./Pages/Listing";
+import CardDetails from "./Pages/CardDetails";
+import Contact from "./Pages/Contact";
+import MyBookings from "./Pages/MyBookings";
+import Home from "./Pages/Home";
+import Footer from "./Components/Footer";
 function App() {
   return (
-    <div className="max-padd-container bg-amber-100">
-      <h1>¡Funciona!</h1>
-      <h2>¡Funciona!</h2>
-    </div>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/listing" element={<Listing />} />
+          <Route path="/listing/:id" element={<CardDetails />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/my-booking" element={<MyBookings />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
   );
 }
 
