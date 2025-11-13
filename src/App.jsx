0f26c56d3f76fe927fb941a53ee6afa404e64483
MarioLivimoro <jcvdbocanegra@gmail.com>
2025-11-12 20:24:02 -0500

    Primer avance

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="max-padd-container bg-amber-100">
+      <h1>¡Funciona!</h1>
+      <h2>¡Funciona!</h2>
+    </div>
+  );
+}
+
+export default App;
