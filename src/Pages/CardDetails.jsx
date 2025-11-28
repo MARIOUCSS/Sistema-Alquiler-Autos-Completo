2460ff3408f5874bbb26737f224bf604f35d1b46
MarioLivimoro <jcvdbocanegra@gmail.com>
2025-11-27 23:47:04 -0500

    agregando funcionalidades

--- src/Pages/CardDetails.jsx
+++ src/Pages/CardDetails.jsx
@@ -1,26 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useMemo } from "react";
 import { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { CartImages } from "../Components/CartImages";
+import CartImages from "../Components/CartImages";
 import { dummyCars } from "../assets/data";
 function CardDetails() {
-  const [car, setCar] = useState(null);
+  //const [car, setCar] = useState(null);
   const { id } = useParams();
-  // const [prickUpdate, setprickUpdate] = useState(null);
-  // const [dropOffDate, setdropOffDate] = useState(null);
-  // const [isAvailable, setisAvailable] = useState(false);
-  // const currency = "$";
-  //const navigate = useNavigate();
+  const [prickUpdate, setprickUpdate] = useState(null);
+  const [dropOffDate, setdropOffDate] = useState(null);
+  const [isAvailable, setisAvailable] = useState(false);
+  const currency = "$";
+  const navigate = useNavigate();
 
-  useEffect(() => {
-    if (dummyCars && dummyCars.length > 0) {
-      const foundcar = dummyCars.find((c) => c._id === id);
-      if (foundcar) {
-        setCar(foundcar);
-      }
-    }
+  // useEffect(() => {
+  //   if (dummyCars && dummyCars.length > 0) {
+  //     const foundcar = dummyCars.find((c) => c._id === id);
+  //     if (foundcar) {
+  //       setCar(foundcar);
+  //     }
+  //   }
+  // }, [dummyCars, id]);
+  const car = useMemo(() => {
+    return dummyCars?.find((c) => c._id === id) || null;
   }, [dummyCars, id]);
-  return <div>{car}</div>;
+  if (!car) {
+    return <div>Car not found</div>;
+  }
+  return <div className="mt-20">{car._id}</div>;
 }
 
 export default CardDetails;
