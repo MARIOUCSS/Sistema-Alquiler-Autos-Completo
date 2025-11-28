ef25b75561ee013e835327f1314404bcf38414b2
MarioLivimoro <jcvdbocanegra@gmail.com>
2025-11-27 23:34:29 -0500

    modificacion del carro

--- src/Pages/CardDetails.jsx
+++ src/Pages/CardDetails.jsx
@@ -1,7 +1,26 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { CartImages } from "../Components/CartImages";
+import { dummyCars } from "../assets/data";
 function CardDetails() {
-  return <div>CardDetails</div>;
+  const [car, setCar] = useState(null);
+  const { id } = useParams();
+  // const [prickUpdate, setprickUpdate] = useState(null);
+  // const [dropOffDate, setdropOffDate] = useState(null);
+  // const [isAvailable, setisAvailable] = useState(false);
+  // const currency = "$";
+  //const navigate = useNavigate();
+
+  useEffect(() => {
+    if (dummyCars && dummyCars.length > 0) {
+      const foundcar = dummyCars.find((c) => c._id === id);
+      if (foundcar) {
+        setCar(foundcar);
+      }
+    }
+  }, [dummyCars, id]);
+  return <div>{car}</div>;
 }
 
 export default CardDetails;
